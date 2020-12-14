43110dc3b975ba4fbd31bb6d8befef7620659cd5
Horstwursti <Horstwursti@users.noreply.github.com>
2020-12-14 11:17:39 +0100

    repaired luna png file path

--- tiny_pixel_luna.tsx
+++ tiny_pixel_luna.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="tiny_pixel_luna" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="../../tiny_pixel_luna.png" width="64" height="64"/>
+ <image source="tiny_pixel_luna.png" width="64" height="64"/>
 </tileset>
