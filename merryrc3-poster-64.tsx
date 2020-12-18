a0bc006f0c533a702473d5d5ec229d9ac51f9fac
BjWe <mail.github@server-place.org>
2020-12-18 10:49:17 +0100

    merge files and cleanup

--- merryrc3-poster-64.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="merryrc3-poster-64" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="merryrc3-poster-64.png" width="64" height="64"/>
-</tileset>
